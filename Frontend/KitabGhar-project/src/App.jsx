6a9c665e0d98eb43e8c6a6be063f5d0b0d24761a
Bashyalhari583 <hbashyal878@gmail.com>
2025-08-15 11:47:21 +0545

    generate navbar

--- Frontend/KitabGhar-project/src/App.jsx
+++ Frontend/KitabGhar-project/src/App.jsx
@@ -1,5 +1,10 @@
 import React from "react";
+import Navbar from "./components/Navbar";
 
-export default function App() {
-  return <div>Hello Coder</div>;
-}
+function App() {
+  return (<>
+    <Navbar />
+    </>
+)}
+
+export default App;
