6b07c56b815f705687b65387164c42d7367887e2
Bashyalhari583 <hbashyal878@gmail.com>
2025-08-17 22:04:25 +0545

    this is course part

--- Frontend/KitabGhar-project/src/App.jsx
+++ Frontend/KitabGhar-project/src/App.jsx
@@ -1,17 +1,21 @@
 import React from "react";
-import Navbar from "./components/Navbar";
-import Banner from "./components/Banner";
-import Footer from "./components/Footer";
-import Freebook from "./components/Freebook";
+import Home from "./home/Home";
+import { Routes,Route } from "react-router-dom";
+import Courses from "./courses/Courses";
+
 
 function App() {
   return (
   <>
-    <Navbar />
-    <Banner />
-    <Freebook />
+  {/* <Home />
+  <Course /> */}
 
-    <Footer />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/course" element={<Courses />} />
+      {/* Add more routes as needed */}
+    </Routes>
+
     </>
 )}
 
